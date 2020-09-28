03d6ae791bf864158206c10266036798ddd507aa
Darius Dzien <ddzien2@gmail.com>
2020-09-27 21:47:15 -0500

    Remove binding.prys and add README

---
+++ README.md
@@ -0,0 +1,6 @@
+# Most common path exercise
+
+## Exercise directions
+
+* Each row in `/lib/support/paths2.txt` represents a path
+* Find the most common path of length `4` of all the rows in `paths2.txt`
--- lib/ruby_challenge.rb
+++ lib/ruby_challenge.rb
@@ -3,7 +3,7 @@ require "pry"
 class RubyChallenge
   def initialize
     @paths = []
-    file = File.open(File.dirname(__FILE__) + "/paths2.txt").read
+    file = File.open(File.dirname(__FILE__) + "/support/paths2.txt").read
     file.each_line do |line|
       new_line = line.chomp.split(",")
       if new_line.length > 3
@@ -15,16 +15,13 @@ class RubyChallenge
   def find_most_common_path
     new_arr = []
     @paths.each do |path|
-      # binding.pry
       path.each_cons(4).to_a.each do |elem|
         new_arr << elem
       end
     end
-    # binding.pry
     result_hash = {}
     new_arr.each do |path|
       path_key = path.join("")
-      # binding.pry
       if result_hash.key?(path_key)
         result_hash[path_key] += 1
       else
