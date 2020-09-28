72eb3506be7390b8f0683cb699f877ff4e1865d0
Darius Dzien <ddzien2@gmail.com>
2020-09-27 21:04:27 -0500

    WIP Add challenge

--- lib/ruby_challenge.rb
+++ lib/ruby_challenge.rb
@@ -1,13 +1,39 @@
 require "pry"
 
 class RubyChallenge
-  attr_reader :test1
-
-  def initialize(test1)
-    @test1 = test1
+  def initialize
+    @paths = []
+    file = File.open(File.dirname(__FILE__) + "/paths2.txt").read
+    file.each_line do |line|
+      new_line = line.chomp.split(",")
+      if new_line.length > 3
+        @paths << new_line
+      end
+    end
   end
 
-  def hello
-    "This is #{@test1}"
+  def find_most_common_path
+    new_arr = []
+    @paths.each do |path|
+      # binding.pry
+      path.each_cons(4).to_a.each do |elem|
+        new_arr << elem
+      end
+    end
+    # binding.pry
+    result_hash = {}
+    new_arr.each do |path|
+      path_key = path.join("")
+      # binding.pry
+      if result_hash.key?(path_key)
+        result_hash[path_key] += 1
+      else
+        result_hash[path_key] = 1
+      end
+    end
+    p result_hash.sort_by {|key, value| value}
   end
 end
+
+a = RubyChallenge.new
+a.find_most_common_path
